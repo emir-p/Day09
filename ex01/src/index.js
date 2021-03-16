3ae4b2f5e4299bdcb1a297c135d6531096a686eb
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-16 10:15:35 +0100

    Boolean Values

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function myTrueBoolean() {
+    return true;
+}
+// Only change code above this line
+console.log(myTrueBoolean());
+module.exports = myTrueBoolean;
